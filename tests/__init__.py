aa9ac516c045f94cc739ba468229b95785e73ee6
Kenneth Leine Schulstad <kls@rubberduck.no>
2024-09-13 22:13:56 +0200

    NO_COLOR

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 """Test suite for the dp package."""
+import os
 
 from rich.console import Console
 
+os.environ["NO_COLOR"] = "1"
 Console(force_terminal=False)  # Disable ANSI colors during testing
