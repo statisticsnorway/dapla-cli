e7d0652583dd14a9cc119a7599735ff412ccbd69
Kenneth Leine Schulstad <kls@rubberduck.no>
2024-09-19 11:08:49 +0200

    Add --version switch

--- src/dp/main.py
+++ src/dp/main.py
@@ -1,6 +1,8 @@
+import importlib.metadata
 import logging
+from typing import Annotated
 
-from typer import Typer
+import typer
 
 from . import auth, lab
 
@@ -8,6 +10,27 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-app = Typer()
+app = typer.Typer()
 app.add_typer(auth.app, name="auth", help="Authenticate dp with Keycloak")
 app.add_typer(lab.app, name="lab", help="Interact with Dapla Lab services")
+
+
+def version_callback(value: bool) -> None:
+    """Print the version."""
+    if value:
+        try:
+            app_version = importlib.metadata.version("dp")
+        except importlib.metadata.PackageNotFoundError:
+            app_version = "dev"
+        print(f"Dapla CLI {app_version}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Annotated[
+        bool | None, typer.Option("--version", callback=version_callback)
+    ] = None,
+) -> None:
+    """Entrypoint for the Dapla CLI."""
+    pass
