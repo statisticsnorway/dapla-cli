3ab3ba0c8c077784668baf3b905c9979062410e6
Kenneth Leine Schulstad <kls@rubberduck.no>
2024-09-20 14:36:20 +0200

    Add command for pruning services. (#12)
    * Functionality for pruning services.
    
    If a service has been running for more than `kill_threshold' hours, it is killed.
    If a service has been suspended for more than `kill_suspended_threshold' hours, it is killed.
    If a service has status 'failed', it is killed.
    If a service has been updated more than `suspend_threshold' hours ago, it is suspended.
    
    
    Also refactor processing of services, extract common util functions and update tests
    
    * Update doc
    
    * Remove test
    
    * Remove invalid test

--- src/dp/utils.py
+++ src/dp/utils.py
@@ -1,12 +1,25 @@
 import re
+import subprocess
+from datetime import datetime, timezone
 from typing import Any
 
+import typer
+from pydantic import BaseModel
+from rich import print
 from rich.console import Console
 from typer import Typer
 
 err = Console(stderr=True)
 ansi_escape = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
 app = Typer()
+
+
+class RunResult(BaseModel):
+    """The result of running a shell command."""
+
+    stdout: str
+    stderr: str
+    returncode: int
 
 
 def red(text: Any) -> str:
@@ -32,3 +45,52 @@ def print_err(text: Any) -> None:
 def strip_ansi(text: str) -> str:
     """Strip ANSI escape sequences from text."""
     return ansi_escape.sub("", text)
+
+
+def hours_since(dt: datetime) -> int:
+    """Calculate the number of hours since a given datetime."""
+    # delta = datetime.now(timezone.utc) - dateutil.parser.isoparse(dt)
+    delta = datetime.now(timezone.utc) - dt
+    return int(delta.total_seconds() // 3600)
+
+
+def run(command: str, dryrun: bool = False, verbose: bool = False) -> RunResult:
+    """Run a shell command.
+
+    Args:
+        command (str): The shell command to run.
+        dryrun (bool): Whether to perform a dry run.
+        verbose (bool): Whether to print the command.
+
+    Returns:
+        RunResult: The result of the command.
+    """
+    if verbose:
+        print(grey(f"{'DRYRUN: ' if dryrun else ''}{command}"))
+
+    if dryrun:
+        return RunResult(stdout="", stderr="", returncode=0)
+
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
+    return RunResult(
+        stdout=result.stdout, stderr=result.stderr, returncode=result.returncode
+    )
+
+
+def assert_successful_command(cmd: str, err_msg: str, success_msg: str | None) -> None:
+    """Run a shell command and assert its success.
+
+    Args:
+        cmd (str): The shell command to run.
+        err_msg (str): The error message to display if the command fails.
+        success_msg (str): The success message to display if the command succeeds.
+
+    Raises:
+        Exit: If the command fails.
+    """
+    res = run(f"eval {cmd} > /dev/null")
+    if res.returncode != 0:
+        err.print(f"❌  {red(err_msg)}")
+        raise typer.Exit(code=res.returncode)
+    if success_msg:
+        print(f"✔️ {success_msg}")
