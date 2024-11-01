7c9de91909affbc378370703d010460d0f63b760
skykanin <3789764+skykanin@users.noreply.github.com>
2024-11-01 14:01:13 +0100

    fix formatting

--- flake.nix
+++ flake.nix
@@ -10,21 +10,33 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, poetry2nix, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+  outputs = inputs @ {
+    flake-parts,
+    poetry2nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
         devShells.default = pkgs.mkShell {
           name = "dapla-lab";
-          packages = [ self'.packages.dapla-cli ];
+          packages = [self'.packages.dapla-cli];
         };
         formatter = pkgs.alejandra;
         packages.dapla-cli = let
-          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication overrides;
-          in mkPoetryApplication {
+          inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication overrides;
+        in
+          mkPoetryApplication {
             projectDir = ./.;
             overrides = overrides.withDefaults (final: prev: {
-              ruff = prev.ruff.override { preferWheel = true; };
+              ruff = prev.ruff.override {preferWheel = true;};
               ruamel-yaml-clib = null;
               sphinx-click = null;
               furo = null;
